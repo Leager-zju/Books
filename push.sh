58025cff3197ccfc0b1bacd064fda15c75078ef9
leagerxu <leagerxu@tencent.com>
2024-08-24 09:44:05 +0800

    [#10] update script to output book name in each commit

--- push.sh
+++ push.sh
@@ -1,1 +1,17 @@
-git add . && git commit -m "update" && git push
+#!/bin/zsh
+
+git add .
+
+commit_number=$(git rev-list --count HEAD)
+pdf_files=$(git diff --cached --name-only | grep .pdf | xargs -I % echo -e % | while read line; do echo $line; done)
+
+commit_message="[#$commit_number] Add Books:"
+for file in $pdf_files; do
+  commit_message="$commit_message\n$file"
+done
+
+echo ${commit_message}
+
+git commit -m "$commit_message"
+
+git push
