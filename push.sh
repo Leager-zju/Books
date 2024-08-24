5e11469c40e43a9be60893f0950428bc7e840b52
leagerxu <leagerxu@tencent.com>
2024-08-24 10:52:30 +0800

    [#14] update script

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ git add .
 commit_number=$(git rev-list --count HEAD)
 pdf_files=$(git diff --cached --name-only | grep .pdf | xargs -I % echo -e % | while read line; do echo $line; done)
 
-commit_message="[#$commit_number] Add Books:"
+commit_message="[#$commit_number] Add Books"
 for file in $pdf_files; do
   commit_message="$commit_message\n$file"
 done
